b53ea3a1666f39a2e3c03fb2e11d72319728f0c6
pro-coder520 <iremideadeyanju9@gmail.com>
2026-01-22 01:41:44 +0100

    Edited the number of allowed hosts

--- agrosphere/settings.py
+++ agrosphere/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['agrosphere-0v26.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
